d78aa83bb6668be04b5ba299837ca9f6ba9187ef
Ruben Cancino <luxsoft.cancino@gmail.com>
2020-02-14 13:03:26 -0600

    @angular/material dependencies

--- apps/cxc/src/app/app.module.ts
+++ apps/cxc/src/app/app.module.ts
@@ -3,12 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Pruebas iniciales
+import { MatSliderModule } from '@angular/material/slider';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabled' })
+    MatSliderModule,
+    MatButtonModule,
+    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
